58fad0ba95289d9fa85b8fe0d9c96eb950876ded
Animesh-Ghosh <ani101098@yahoo.in>
2022-09-11 18:27:32 +0530

    Add tests for utility functions
    Created test suite for functions in utils.py.
    Added non_zero_padded_prefix function to return the platform-specific
    prefix for getting non-zero-padded numbers in utils.py.
    Updated README with instructions to run the test suite.

--- utils.py
+++ utils.py
@@ -1,22 +1,36 @@
 import time
 from datetime import datetime, timedelta
+from sys import platform
+
+
+def non_zero_padded_prefix():
+    """Returns the platform specific prefix for the non-zero-padded numbers."""
+    prefix = "-"
+    if platform.startswith("win32"):
+        prefix = "#"
+
+    return prefix
+
 
 def get_last_month():
     now = datetime.now()
     last_month = now - timedelta(weeks=4)
-    return last_month.strftime("%-m %Y")
+    return last_month.strftime(f"%{non_zero_padded_prefix()}m %Y")
+
 
 def get_last_day():
     now = datetime.now()
     last_day = now - timedelta(days=1)
-    return last_day.strftime("%d/%-m/%Y")
+    return last_day.strftime(f"%d/%{non_zero_padded_prefix()}m/%Y")
+
 
 def get_readable_month(unix_t):
     t = time.localtime(unix_t)
     # Format: '9, 2021'
-    return time.strftime("%-m %Y", t)
+    return time.strftime(f"%{non_zero_padded_prefix()}m %Y", t)
+
 
 def get_readable_day(unix_t):
     t = time.localtime(unix_t)
     # Format: '03/12/2021'
-    return time.strftime("%d/%-m/%Y", t)
+    return time.strftime(f"%d/%{non_zero_padded_prefix()}m/%Y", t)
