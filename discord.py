f843cba508ca35589b23959d5b4dca60b3c371aa
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2024-04-26 14:15:44 +0000

    Added typing

--- discord.py
+++ discord.py
@@ -1,16 +1,21 @@
-import httpx
 from typing import Dict, Any
+import httpx
 
-DISCORD_INVITE_HACK_URL = 'https://discord.com/api/v9/invites/{invite_code}?with_counts=true&with_expiration=true'
+DISCORD_INVITE_HACK_URL = "https://discord.com/api/v9/invites/{invite_code}?with_counts=true&with_expiration=true"
+
 
 def get_discord_stats(invite_code: str) -> Dict[str, Any]:
-    discord_stats = {}
+    """Get discord stats from invite code"""
+
+    discord_stats: Dict[str, Any] = {}
     res = httpx.get(DISCORD_INVITE_HACK_URL.format(invite_code=invite_code))
     if res.status_code == 200:
         res_json = res.json()
-        discord_stats['discordTotalMembers'] = res_json.get('approximate_member_count')
-        discord_stats['discordTotalActiveMembers'] = res_json.get(
-            'approximate_presence_count'
+        discord_stats["discordTotalMembers"] = res_json.get(
+            "approximate_member_count", 0
+        )
+        discord_stats["discordTotalActiveMembers"] = res_json.get(
+            "approximate_presence_count", 0
         )
 
     return discord_stats
